be954a6fecb7a5627185fec3e2d799e4807f1ac2
Kuperdyai <kuperdyai@yandex.ru>
2022-11-02 11:29:27 +0300

    added the project

---
+++ AllProff/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllProff
+//
+//  Created by Александр on 01.11.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
